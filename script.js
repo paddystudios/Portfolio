460089849ba856bac12b98415fc7e7e32e788cbd
Mohamed Guled <mohamed.guled@hyperisland.se>
2025-05-10 20:33:24 +0200

    Portfolio, finished

---
+++ script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    spaceBetween: 20,
+    centeredSlides: false,
+    loop: false,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  
